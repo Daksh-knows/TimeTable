4549e30fb84ecb771c15f3f709ed178b906adfb8
Daksh-knows <145273202+Daksh-knows@users.noreply.github.com>
2024-11-11 15:04:05 +0530

    Create config.py

---
+++ FLaskServer/config.py
@@ -0,0 +1,2 @@
+AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=jainamkhanter;AccountKey=Tmjco/UPi1cjSsrEurSk8gqtmsszKf8d6bYkMjXk2lqI3DHVJabm50kcMJ7i5XyqlvPRs1y0lmiZ+AStJ5U+3Q==;EndpointSuffix=core.windows.net"
+AZURE_CONTAINER_NAME = "notes"
